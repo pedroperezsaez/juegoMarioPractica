f45cde942bda1944937822a10f7d460c1f4667f1
pedroperezsaez <pedroperezsaez.1992@gmail.com>
2025-11-26 16:52:24 +0100

    con los cambios hechos de pere

--- main.js
+++ main.js
@@ -1,197 +1,271 @@
 'use strict'
 
-let canvas=document.getElementById('canvas');
-let ctx= canvas.getContext('2d');
-let background=null;
-let playerImg=null;
-let scrollH=0;
-let scrollV=0;
-let frames=0;
-let playerDirection={};
-let velocidadCanvas={};
-let animacion=0;
-let playerPositionY=192;
-let saltando=false;
-let saltoAltura=0;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let background = null;
+let playerImg = null;
+let scrollH = 0;
+let scrollV = 0;
+let frames = 0;
+let playerDirection = {};
+let velocidadCanvas = {};
+let animacion = 0;
+let playerPositionY = 192;
+let saltando = false;
+let saltoAltura = 0;
+let collision_object={
+    x: 447,
+    y:175,
+    w:480-447,
+    h:209-175,
+}
 
-function getKeys(){
-    let keys={};
-    window.onkeydown=function(e){
-        keys[e.key]=true;
+function getKeys() {
+    let keys = {};
+    window.onkeydown = function (e) {
+        keys[e.key] = true;
     }
-    window.onkeyup=function(e){
-        keys[e.key]=false;
+    window.onkeyup = function (e) {
+        keys[e.key] = false;
     }
     return keys;
 }
-let keys=getKeys();
+let keys = getKeys();
 
-function loadImage(url){
-    let img= new Image();
-    img.src=url;
-    return new Promise(function (resolve,reject) {
-        img.onload=function(){
+function loadImage(url) {
+    let img = new Image();
+    img.src = url;
+    return new Promise(function (resolve, reject) {
+        img.onload = function () {
             resolve(img)
         }
     })
 }
-let fondoJuego={
-    posicionY:256,
-    posicionX:240,
+let fondoJuego = {
+    posicionY: 256,
+    posicionX: 240,
 }
-let player={
-    posx:fondoJuego.posicionX/2,
-    posy:192,
-    width:40,
-    height:50,
+let player = {
+    posx: fondoJuego.posicionX / 2,
+    posy: 192,
+    width: 18,
+    height:18,
    
+    onground:true,
+    dir: 'Q',
+    vy:0,
+
 }
 
 
 
- 
 
-let marioIzqu={
-    posx:177,
-    posy:88
+
+let marioIzqu = {
+    posx: 177,
+    posy: 88
 }
-let marioDer={
-    posx:216,
-    posy:88,
+let marioDer = {
+    posx: 216,
+    posy: 88,
 }
-let marioCaminandoDer={
-    posx:255,
-    posy:88,
+let marioCaminandoDer = {
+    posx: 255,
+    posy: 88,
 }
-let marioCaminandoIzq={
- posx:134,
-    posy:88,
+let marioCaminandoIzq = {
+    posx: 134,
+    posy: 88,
 };
-playerDirection=marioDer;
+playerDirection = marioDer;
 
-function marioMovimientoDer(){    
-    if(animacion<10){
-        playerDirection=marioDer;
-      
-    }else if(animacion>=10 && animacion<20){
-        playerDirection=marioCaminandoDer;
-    }else{
-        animacion=0;
+function marioMovimientoDer() {
+    if (animacion < 10) {
+        playerDirection = marioDer;
+
+    } else if (animacion >= 10 && animacion < 20) {
+        playerDirection = marioCaminandoDer;
+    } else {
+        animacion = 0;
+    }
+    if(!keys['ArrowRight']){
+        playerDirection = marioDer;
+        player.dir = 'R'
     }
 }
 
-function marioMovimientoIzq(){
+function marioMovimientoIzq() {
+
+    if (animacion < 10) {
+        playerDirection = marioIzqu;
+
+    } else if (animacion >= 10 && animacion < 20) {
+        playerDirection = marioCaminandoIzq;
+    } else {
+        animacion = 0;
+    }
     
-    if(animacion<10){
-        playerDirection=marioIzqu;
-      
-    }else if(animacion>=10 && animacion<20){
-        playerDirection=marioCaminandoIzq;
-    }else{
-        animacion=0;
-    }
 }
 
-function salto(){
-        if(!saltando){
-            saltando= true;
-            saltoAltura=-5;
+/*function salto() {
+    if (!saltando) {
+        saltando = true;
+        saltoAltura = -5;
 
-        }
-        
     }
-    //tengo que seguir mirando aqui
-    let alturaMaximaDeSalto = 100;
-    function saltoYBajada(){       
-         player.posy=player.posy+saltoAltura;
-        console.log(player.posy)
 
-        if(player.posy<=alturaMaximaDeSalto){ 
-             player.posy = playerPositionY;
-            saltando = false;
-            saltoAltura = 0;
-        }   
-        
-        
+}
+//tengo que seguir mirando aqui
+let alturaMaximaDeSalto = 100;
+function saltoYBajada() {
+    player.posy = player.posy + saltoAltura;
+    console.log(player.posy)
+
+    if (player.posy <= alturaMaximaDeSalto) {
+        player.posy = playerPositionY;
+        saltando = false;player
+        saltoAltura = 0;
     }
-  
+
+
+}
+    */
+
 
 
 
 function pararArrancarCanvas() {
-    if (((player.posx) - (fondoJuego.posicionX / 2))<0) {
-       player.posx+=1
+    if (((player.posx) - (fondoJuego.posicionX / 2)) < 0) {
+        player.posx += 1
     } else {
-        scrollH += 1;   
+        scrollH += 1;
     }
 }
-
-
-function update(){
-
-if(keys['ArrowLeft']){
-   marioMovimientoIzq();
-   player.posx-=1;
+function collisions(){
+let xmina = player.posx
+let xmaxa = xmina + player.width
+let ymina=player.posy
+let ymaxa=ymina+player.height
+let xminb=collision_object.x
+let xmaxb= xminb+collision_object.w
+let yminb= collision_object.y
+let ymaxb= yminb+collision_object.h
+if(ymaxa<yminb ||ymaxb < ymina) return false;
+if(xmaxa < xminb || xmaxb< xmina) return false;
+return true;
 }
 
-if(keys['ArrowRight']){
-   marioMovimientoDer()
- pararArrancarCanvas();
+function update() {
+
+if(collisions()){
+    console.log("colision")
+   player.posx= collision_object.x - player.width-0.1
 }
-if(keys['ArrowUp']){
-    salto();
-   
+
+    player.dir = 'Q'
+    if (keys['ArrowLeft']) {
+        player.dir = 'L'
+        marioMovimientoIzq();
+        player.posx -= 1;
+    }
+
+    if (keys['ArrowRight']) {
+        player.dir = 'R'
+        marioMovimientoDer()
+        pararArrancarCanvas();
+        
+       
+    }
+    if (keys['ArrowUp']) {
+        /*salto();
+        */
+       player.posy=player.posy-2;
+       if (player.onground){
+        player.vy =-6.4
+        player.onground=false
+       }
+
+       if(!player.onground){
+        player.vy+=0.3
+       }
+       player.posy+=player.vy;
+
+       if(player.posy >= 192){
+        player.posy=192
+        player.onground=true
+        player.vy=0
+       }
+
+
+    }
     
-}
-if(keys['ArrowDown']){
-    player.posy+=5
-}
-if(keys['z']){
-    velocidadCanvas.izquierda;
-}
-if(keys['x']){
-    scrollH+=2
-}
-if(keys['q']){
-    scrollV-=2
-}
-if(keys['a']){
-    scrollV+=2
-}
+    if (keys['z']) {
+        velocidadCanvas.izquierda;
+    }
+    if (keys['x']) {
+        scrollH += 2
+    }
+    if (keys['q']) {
+        scrollV -= 2
+    }
+    if (keys['a']) {
+        scrollV += 2
+    }
 
 }
 
-function draw(){
+function draw() {
     //esto es el fondo
-   ctx.drawImage(background,scrollH,scrollV,fondoJuego.posicionY,fondoJuego.posicionX,0,0, fondoJuego.posicionY,fondoJuego.posicionX)
+  
+    ctx.drawImage(background, scrollH, scrollV, fondoJuego.posicionY, fondoJuego.posicionX, 0, 0, fondoJuego.posicionY, fondoJuego.posicionX)
 
-   //personaje
-    ctx.drawImage(playerImg, playerDirection.posx, playerDirection.posy, 18,18, player.posx, player.posy, 18,18 )
-   //ctx.beginPath();
+    //personaje
+    ctx.drawImage(playerImg, playerDirection.posx, playerDirection.posy, 18, 18, player.posx, player.posy, 18, 18)
+    //ctx.beginPath();
     //ctx.rect(player.posx,player.posy,player.width,player.height);
     //ctx.fill();
+    
 }
-function mainLoop(){
-    saltoYBajada()
-   animacion++;
+
+const FRAMETIME = 16;
+let startTime = null;
+let lag=0;
+
+function mainLoop(timestamp) {
+    if (timestamp === undefined) {
+        requestAnimationFrame(mainLoop);
+        return;
+    }
+    if (startTime == null) startTime = timestamp
+    let delta = timestamp - startTime
+    startTime = timestamp
+    lag+=delta;
+
+    while (lag > FRAMETIME) {
+        update();
+        lag -= FRAMETIME;
+    }
+
+    //saltoYBajada()
+    animacion++;
     frames++;
     //Actualitzar el mon (entitats, controls..)
-    update();
+
     //Dibuixar els elements dle joc
     ctx.save()
-    ctx.scale(2.5,2.5)
+    ctx.scale(2.5, 2.5)
     draw();
     ctx.restore();
     requestAnimationFrame(mainLoop);
 }
 
-async function main(){
-// inicialitzacions
-background = await loadImage('imgs/bg-1-1.png')
-playerImg=await loadImage('imgs/mario.png')
+async function main() {
+    // inicialitzacions
+    background = await loadImage('imgs/bg-1-1.png')
+    playerImg = await loadImage('imgs/mario.png')
 
-//bucl principal(60fps)
-mainLoop()
+    //bucl principal(60fps)
+    mainLoop()
 }
 
 main();
